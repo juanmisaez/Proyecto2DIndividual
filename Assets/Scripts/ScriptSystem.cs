cc7b54c7e055076c6a60e911aa2d8f9676adb0d8
juanmisaez <juanmiguelsaezh@gmail.com>
2022-01-23 16:34:35 +0100

    Version 3.5
    Sistema de guion terminado

--- Assets/Scripts/ScriptSystem.cs
+++ Assets/Scripts/ScriptSystem.cs
@@ -10,7 +10,8 @@ public class ScriptSystem : MonoBehaviour
     public event Action<int> SelectDialogue = delegate { }; // Al CutsceneManager
 
     private bool bag;
-    private int phase;       
+    private int phase;
+    private int beforePhase;
 
     /*
     public class CollisionCutscene : CollisionSystem
@@ -41,43 +42,62 @@ public class ScriptSystem : MonoBehaviour
         {
             Debug.Log("FASE 0, intro");
             SelectDialogue(phase);
-            PhaseUp();
+            NextPhase();
         }
-        else if(!bag) // Mochila vacía o < 3
+        else if(!bag) // Mochila no llena del todo
         {
-            Debug.Log("FASE 1/2/3, mochila vacía");
+            Debug.Log("FASE 1, mochila vacía");
             SelectDialogue(phase);
         }
-        else if(phase == 1 && bag) // Primer pedido realizado
+        else if(phase == 2 && bag) // Primer pedido realizado
         {
-            Debug.Log("FASE 1, primer pedido completo");
-            SelectDialogue(phase);
-            BagEmpty();
-        }
-        else if (phase == 2 && bag) // Segundo pedido realizado
-        {
-            Debug.Log("FASE 2, segundo pedido completo");
+            Debug.Log("FASE 2, primer pedido completo");
             SelectDialogue(phase);
             BagEmpty();
+            SavePhase(phase);
+            ReturnPhase();
+            //**desbloquear el romper bloques azules**
         }
-        else if (phase == 3 && bag) // Tercer y último pedido realizado
+        else if (phase == 3 && bag) // Segundo pedido realizado
         {
-            Debug.Log("FASE 3, tercer pedido completo");
+            Debug.Log("FASE 3, segundo pedido completo");
             SelectDialogue(phase);
-            //BagEmpty();
+            BagEmpty();
+            SavePhase(phase);
+            ReturnPhase();
+            //**desbloquear el romper bloques metálicos**
+        }
+        else if (phase == 4 && bag) // Tercer y último pedido realizado
+        {
+            Debug.Log("FASE 4, tercer pedido completo");
+            SelectDialogue(phase);
+            BagEmpty();
         }
     }
 
-    void PhaseUp()
+    void NextPhase()
     {
-        phase += 1;
+        //phase += 1;
+        phase += beforePhase;
+        UpdatePhase(phase);
+    }
+
+    void SavePhase(int currentPhase)
+    {
+        beforePhase = currentPhase;
+        Debug.Log("la fase anterior es " + beforePhase); //---
+    }
+
+    void ReturnPhase()
+    {
+        phase = 1; // fase de la mochila vacía
         UpdatePhase(phase);
     }
 
     void BagFull()
     {
         bag = true; // al llenarse la mochila
-        PhaseUp();
+        NextPhase();
     }
 
     void BagEmpty()
@@ -88,6 +108,8 @@ public class ScriptSystem : MonoBehaviour
 
     void OnEnable()
     {
+        beforePhase = 1;
+
         InventorySystem.InventoryFull += BagFull;
         PhaseSystem.SearchPhase += PhaseSelection;
     }
